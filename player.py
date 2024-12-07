1bd26bd0e21c6404118209df0c39a63af0c31b4e
joe-robinson <joe@rbnsn.uk>
2024-12-07 08:06:31 +0000

    Add player entity with drawing functionality
    Introduce a `Player` class derived from `CircleShape` with methods to define and draw its triangular shape. Update `main.py` to include the player in the game loop, positioning it at the center of the screen. Update `constants.py` to define `PLAYER_RADIUS`, supporting the player's size attribute.

---
+++ player.py
@@ -0,0 +1,28 @@
+import pygame
+
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.radius = PLAYER_RADIUS
+        self.x_pos = x
+        self.y_pos = y
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        points = self.triangle()
+        color = "white"
+        width = 2
+        return pygame.draw.polygon(screen, color, points, width)
